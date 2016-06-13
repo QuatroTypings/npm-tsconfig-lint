a1a441510a6d9a0b2ecc21920531b7258f291742
Giedrius Grabauskas <giedrius@quatrodev.com>
2016-06-13 12:02:45 +0300

    Removed contracts file and all context moved to index.d.ts.  … Updated test file.

--- 0.11.0/index.d.ts
+++ 0.11.0/index.d.ts
@@ -2,6 +2,44 @@
 // Project: https://github.com/wjohnsto/tsconfig-lint
 // Definitions by: Giedrius Grabauskas <https://github.com/GiedriusGrabauskas>
 
-import * as tsconfigLint from './contracts';
-declare var _tsconfigLint: tsconfigLint.tsConfig;
-export = _tsconfigLint;
+declare namespace tsconfigLint {
+    interface Options {
+        /**
+         * A relative path from cwd to the directory containing a tsconfig.json. If not specified, the '.' is used.
+         */
+        configPath?: string;
+
+        /**
+         * The current working directory, defaults to `process.cwd()`
+         */
+        cwd?: string;
+
+        /**
+         * Whether or not `filesGlob` should be used in place of `files` for determining the files to lint.
+         */
+        useGlob?: boolean;
+
+        tsconfigOptions?: {
+            /**
+             * The number of spaces to indent the tsconfig.json
+             */
+            indent?: number;
+        };
+
+        /**
+         * A relative path from the configPath to the tslint configuration file.
+         */
+        tsLintConfigFilePath?: string;
+
+    }
+
+    interface CallBack {
+        (err: any): void;
+    }
+
+    export interface tsConfig {
+        (options: Options, errorCallback: CallBack): void;
+    }
+}
+declare var tsconfigLint: tsconfigLint.tsConfig;
+export = tsconfigLint;
